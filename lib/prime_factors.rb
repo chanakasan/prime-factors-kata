d723b85daafe25d4ac202d70118531d66f0cc7ad
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-06-07 00:49:24 +0530

    Fourth test passes

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -1,23 +1,20 @@
 class PrimeFactors
-  PRIMES = [2,3,5,7,11]
 
   def self.generate(input)
     result = []
 
     if input > 1
-      PRIMES.each do |prime_num|
-        is_divisible = input % prime_num === 0
-        result.push(prime_num) if is_divisible
+      if input % 2 === 0
+        result.push(2)
+        input = input / 2
+      end
 
-        if is_divisible and input / prime_num > 1
-          PRIMES.each do |prime_num2|
-            is_divisible = input % prime_num2 === 0
-            result.push(prime_num2) if is_divisible
-          end
-        end
+      if input > 1
+        result.push(input)
       end
     end
 
     result
   end
+
 end

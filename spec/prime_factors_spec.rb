449368fde82f4fbb6900c8a290d20881ce90d0ea
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-06-07 13:28:45 +0530

    Verification test

--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -31,5 +31,22 @@ describe PrimeFactors do
     it "returns [3,3] for 9" do
       expect(PrimeFactors.generate(9)).to eq([3,3])
     end
+
+    it "returns same results as Ruby's built-in Prime numbers support" do
+      expect(PrimeFactors.generate(10)).to eq(ruby_prime_factorization(10))
+      expect(PrimeFactors.generate(16)).to eq(ruby_prime_factorization(16))
+      expect(PrimeFactors.generate(64)).to eq(ruby_prime_factorization(64))
+      expect(PrimeFactors.generate(100)).to eq(ruby_prime_factorization(100))
+      expect(PrimeFactors.generate(1000)).to eq(ruby_prime_factorization(1000))
+      expect(PrimeFactors.generate(10000)).to eq(ruby_prime_factorization(10000))
+      expect(PrimeFactors.generate(100000)).to eq(ruby_prime_factorization(100000))
+      expect(PrimeFactors.generate(1000000)).to eq(ruby_prime_factorization(1000000))
+    end
+  end
+
+  require 'prime'
+
+  def ruby_prime_factorization(n)
+    Prime.prime_division(n).flat_map { |factor, power| [factor] * power }
   end
 end
